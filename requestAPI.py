f193757ad253661193f6829dc1cf4d999410c878
caue.lugli <54319415+cauelugli@users.noreply.github.com>
2020-11-12 15:53:55 -0300

    Delete requestAPI.py

--- requestAPI.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-get = requests.get('http://127.0.0.1:5000').status_code
-post = requests.post('http://127.0.0.1:5000/post').status_code
-if get == 200:
-    print("GET")
-if post == 200:
-    print("POST")
